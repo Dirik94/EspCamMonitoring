7de8d064980ff7d1ec5af2c348bca9926e28c697
Greg <grzegorz.domanowski.ais@gmail.com>
2023-01-31 15:55:01 +0100

    ignore cleartext error

--- app/src/main/java/com/example/espcammonitoring/MainActivity.kt
+++ app/src/main/java/com/example/espcammonitoring/MainActivity.kt
@@ -23,6 +23,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
